2f5fc58ca6abb1159431a3acf8273eb7a7c1f920
KaioHPs <kaiohpsalternativo@gmail.com>
2021-07-29 16:12:05 -0300

    Configuraçao incial de Routes e diretorios

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import './App.css';
+import { Switch, Route } from 'react-router-dom';
 import rockGlass from './images/rockGlass.svg';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
@@ -14,6 +15,24 @@ function App() {
       >
         Glass
       </object>
+      <Switch>
+        <Route exact path="/" />
+        <Route exact path="/comidas" />
+        <Route exact path="/bebidas" />
+        <Route exact path="/comidas/:recipeId" />
+        <Route exact path="/bebidas/:recipeId" />
+        <Route exact path="/comidas/:recipeId/in-progress" />
+        <Route exact path="/bebidas/:recipeId/in-progress" />
+        <Route exact path="/explorar" />
+        <Route exact path="/explorar/comidas" />
+        <Route exact path="/explorar/bebidas" />
+        <Route exact path="/explorar/comidas/ingredientes" />
+        <Route exact path="/explorar/bebidas/ingredientes" />
+        <Route exact path="/explorar/comidas/area" />
+        <Route exact path="/perfil" />
+        <Route exact path="/receitas-feitas" />
+        <Route exact path="/receitas-favoritas" />
+      </Switch>
     </div>
   );
 }
