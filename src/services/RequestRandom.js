48ebc749e9845beb8c5f93d835db1974f39cea26
Leandro Lopes <leandro_carlos@uol.com.br>
2021-08-03 15:28:39 -0300

    requisitos 75-76

--- src/services/RequestRandom.js
+++ src/services/RequestRandom.js
@@ -5,9 +5,21 @@ export const getRandomDrink = async () => {
   return drinks[0];
 };
 
+export const getIngredientes = async () => {
+  const { drinks } = await fetch('https://www.thecocktaildb.com/api/json/v1/1/list.php?i=list')
+    .then((response) => response.json());
+  return drinks;
+};
+
 export const getRandomMeal = async () => {
   const { meals } = await fetch('https://www.themealdb.com/api/json/v1/1/random.php')
     .then((response) => response.json());
   console.log(meals);
   return meals[0];
 };
+
+export async function getIngredientsMeals() {
+  const { meals } = await fetch('https://www.themealdb.com/api/json/v1/1/list.php?i=list')
+    .then((data) => data.json());
+  return meals;
+}
