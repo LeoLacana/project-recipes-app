eb543f9087cd4a3682a69035b44f9f3264ca3d12
LeoLacana <wilson17414@gmail.com>
2021-08-04 21:14:39 -0300

    correção

--- src/service/RequestSearchBar.js
+++ src/service/RequestSearchBar.js
@@ -1,36 +1,64 @@
+const messageAlert = () => (
+  // eslint-disable-next-line no-alert
+  alert('Sinto muito, não encontramos nenhuma receita para esses filtros.')
+);
 export const requestIngredient = async (ingrediente, type) => {
   if (type === 'comidas') {
     return fetch(`https://www.themealdb.com/api/json/v1/1/filter.php?i=${ingrediente}`)
       .then((result) => result.json())
-      .then((r) => r.meals);
+      .then((r) => r.meals)
+      .catch(() => {
+        messageAlert();
+        return [];
+      });
   }
   return fetch(`https://www.thecocktaildb.com/api/json/v1/1/filter.php?i=${ingrediente}`)
     .then((results) => results.json())
-    .then((r) => r.drinks);
+    .then((r) => r.drinks)
+    .catch(() => {
+      messageAlert();
+      return [];
+    });
 };
 
 export const requestName = async (nome, type) => {
   if (type === 'comidas') {
     return fetch(`https://www.themealdb.com/api/json/v1/1/search.php?s=${nome}`)
       .then((results) => results.json())
-      .then((r) => r.meals);
+      .then((r) => r.meals)
+      .catch(() => {
+        messageAlert();
+        return [];
+      });
   }
   return fetch(`https://www.thecocktaildb.com/api/json/v1/1/search.php?s=${nome}`)
     .then((results) => results.json())
-    .then((r) => r.drinks);
+    .then((r) => r.drinks)
+    .catch(() => {
+      messageAlert();
+      return [];
+    });
 };
 
 export const requestLetra = async (primeiraLetra, type) => {
   if (type === 'comidas') {
     return fetch(`https://www.themealdb.com/api/json/v1/1/search.php?f=${primeiraLetra}`)
       .then((results) => results.json())
-      .then((r) => r.meals);
+      .then((r) => r.meals)
+      .catch(() => {
+        messageAlert();
+        return [];
+      });
   }
   return fetch(`https://www.thecocktaildb.com/api/json/v1/1/search.php?f=${primeiraLetra}`)
     .then((results) => results.json())
-    .then((r) => r.drinks);
+    .then((r) => r.drinks)
+    .catch(() => {
+      messageAlert();
+      return [];
+    });
 };
-// s
+
 export default {
   requestIngredient,
   requestName,
