e9939bb88b315c546d7a1c4faadb04b71cc9c9fb
LeoLacana <wilson17414@gmail.com>
2021-08-09 23:50:09 -0300

    lint

--- src/pages/RecipesMade.js
+++ src/pages/RecipesMade.js
@@ -2,8 +2,6 @@ import React from 'react';
 import Header from '../components/Header';
 import BtnShare from '../components/BtnShare';
 
-import './RecipesMade.css';
-
 export default function RecipesMade() {
   const recipesDone = JSON.parse(localStorage.getItem('doneRecipes'));
   const a = [];
